2a5d6eb3bee4c658f70ac6a72f95df19ae506a15
Joji-iidaka <joji.kapi.0218@gamil.com>
2024-05-25 09:36:45 +0900

    確認

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,6 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        This project is joji app project.
-      </p>
     </>
   )
 }
